e00b8ffead9a4487d4ec01c674d6be1895b5c087
Ariel Duek <arielduek@gmail.com>
2020-04-30 13:54:37 +0300

    Change Names

--- solver.hpp
+++ solver.hpp
@@ -23,26 +23,23 @@ namespace solver{
         ~ComplexVariable() {}
     };
 
-    class POLI{     //FORM: ar^e+br+c
+    class polinom{     //FORM: ar^e+br+c
     private:
         double a,b,c;
-        int e;
         RealVariable* r;
         bool var;
 
     public:
-        POLI() : a(0), b(0), c(0), e(1), var(false){}            //default Constructor
-        POLI(int c) : a(0), b(0), c(c), e(1), var(false){}
-        POLI(double c) : a(0), b(0), c(c), e(1), var(false){}
-        POLI(RealVariable& r) : a(0), r(&r), b(1), c(0), var(true), e(1) {}     //holds the x Final Resault into LINE
-        POLI(double a, int e) : a(a), b(0), c(0), e(e), var(false) {}        //make form of: x^e into POLI
-        POLI(double a, double b, double c, int e) : a(a), b(b), c(c), e(e), var(false) {}   //copy Constructor
+        polinom() : a(0), b(0), c(0), var(false){}            //default Constructor
+        polinom(int c) : a(0), b(0), c(c), var(false){}
+        polinom(double c) : a(0), b(0), c(c), var(false){}
+        polinom(RealVariable& r) : a(0), r(&r), b(1), c(0), var(true){}     //holds the x Final Resault into LINE
+        polinom(double a, double b, double c) : a(a), b(b), c(c), var(false) {}   //copy Constructor
 //        ~POLI(){}
 
         double getA() { return a; };
         double getB() { return b; };
         double getC() { return c; };
-        int getE() { return e; };
         RealVariable* getX() { return r; };
         void setX(RealVariable* R) { this->r = R; }
         bool haveVar() {return this->var;}
@@ -52,27 +49,24 @@ namespace solver{
 
 //------------------------------------------------------------------------------------------------------------------------
 
-    class POLIcx{     //FORM: ax^e+bx+c
+    class complexPolinom{     //FORM: ax^e+bx+c
     private:
         complex<double> a,b,c;
-        int e;
         ComplexVariable* x;
         bool var;
 
     public:
-        POLIcx(): a(0), b(0), c(0), e(1), var(false){}            //default Constructor
-        POLIcx(int c) : a(0), b(0), c(c), e(1), var(false){}
-        POLIcx(double c) : a(0), b(0), c(c), e(1), var(false){}
-        POLIcx(complex<double> c) : a(0), b(0), c(c), e(1), var(false){}
-        POLIcx(ComplexVariable& x) : a(0), x(&x), b(1), c(0), e(1), var(true) {}     //holds the x Final Resault into LINE
-        POLIcx(complex<double> a, int e) : a(a), b(0), c(0), e(e), var(false) {}        //make form of: x^e into POLI
-        POLIcx(complex<double> a, complex<double> b, complex<double> c, int e) : a(a), b(b), c(c), e(e), var(false) {}   //copy Constructor
-        ~POLIcx(){}
+        complexPolinom(): a(0), b(0), c(0), var(false){}            //default Constructor
+        complexPolinom(int c) : a(0), b(0), c(c), var(false){}
+        complexPolinom(double c) : a(0), b(0), c(c), var(false){}
+        complexPolinom(complex<double> c) : a(0), b(0), c(c), var(false){}
+        complexPolinom(ComplexVariable& x) : a(0), x(&x), b(1), c(0), var(true) {}     //holds the x Final Resault into LINE
+        complexPolinom(complex<double> a, complex<double> b, complex<double> c) : a(a), b(b), c(c), var(false) {}   //copy Constructor
+        ~complexPolinom(){}
 
         complex<double> getA() { return a; };
         complex<double> getB() { return b; };
         complex<double> getC() { return c; };
-        int getE() { return e; };
         ComplexVariable* getX() { return x; };
         void setX(ComplexVariable* X) { this->x = X; }
         bool haveVar() {return this->var;}
@@ -80,48 +74,48 @@ namespace solver{
 
 //------------------------------------------------------------------------------------------------------------------------
 
-    POLI operator ^ (POLI A, int e){
-        POLI R( A.getB(),0, A.getC(), e);
+    polinom operator ^ (polinom A, int e){
+        polinom R( A.getB(),0, A.getC());
         R.setX(A.getX());
         return R;
     }
 
-    POLI operator + (POLI A, POLI B){
-        POLI R(A.getA()+B.getA(), A.getB()+B.getB(), A.getC()+B.getC(), 1);
+    polinom operator + (polinom A, polinom B){
+        polinom R(A.getA()+B.getA(), A.getB()+B.getB(), A.getC()+B.getC());
         if(A.getX()) R.setX(A.getX());
         else R.setX(B.getX());
         return R;
     }
 
-    POLI operator - (POLI A, POLI B){
-        POLI R(A.getA()-B.getA(), A.getB()-B.getB(), A.getC()-B.getC(), 1);
+    polinom operator - (polinom A, polinom B){
+        polinom R(A.getA()-B.getA(), A.getB()-B.getB(), A.getC()-B.getC());
         if(A.getX()) R.setX(A.getX());
         else R.setX(B.getX());
         return R;
     }
 
-    POLI operator * (POLI A, POLI B){  // b1r+c1 * b2r+c2
-        POLI R(0,(A.getC()*B.getB())+(B.getC()*A.getB()), A.getC()*B.getC(),1);
+    polinom operator * (polinom A, polinom B){  // b1r+c1 * b2r+c2
+        polinom R(0,(A.getC()*B.getB())+(B.getC()*A.getB()), A.getC()*B.getC());
         if (A.haveVar()) R.setX(A.getX());
         else R.setX(B.getX());
         return R;
     }
 
-    POLI operator / (POLI A, POLI B){ //  b1*r+c1 / b2*r+c2   ->   (b1/b2)*r+(c1/c2)
+    polinom operator / (polinom A, polinom B){ //  b1*r+c1 / b2*r+c2   ->   (b1/b2)*r+(c1/c2)
         if (B.getC() == 0 && B.getB()==0 && B.getA()==0) throw runtime_error("can not divide by 0!!");
         if (B.getC() == 0 && B.getB()==0) return A;
         if (B.getC() == 0) {
-            POLI R(0, 0, A.getB() / B.getB(), 1);
+            polinom R(0, 0, A.getB() / B.getB());
             R.setX(A.getX());
             return R;
         }
-        POLI R(0, A.getB() / B.getC(), A.getC() / B.getC(), 1);
+        polinom R(0, A.getB() / B.getC(), A.getC() / B.getC());
         R.setX(A.getX());
         return R;
     }
 
-    POLI operator == (POLI A, POLI B){
-        POLI C = A - B;
+    polinom operator == (polinom A, polinom B){
+        polinom C = A - B;
         if (C.getA()==0 && C.getB()==0) {
             if (C.getC()==0){
                 C.getX()->real=0;
@@ -144,51 +138,51 @@ namespace solver{
 
 
 
-    POLIcx operator ^ (POLIcx A, int e){
-        POLIcx R( A.getB(),0, A.getC(), e);
+    complexPolinom operator ^ (complexPolinom A, int e){
+        complexPolinom R(A.getB(), 0, A.getC());
         R.setX(A.getX());
         return R;
     }
 
-    POLIcx operator + (POLIcx A, POLIcx B){
-        POLIcx R(A.getA()+B.getA(), A.getB()+B.getB(), A.getC()+B.getC(), 1);
+    complexPolinom operator + (complexPolinom A, complexPolinom B){
+        complexPolinom R(A.getA() + B.getA(), A.getB() + B.getB(), A.getC() + B.getC());
         if(A.getX()) R.setX(A.getX());
         else R.setX(B.getX());
         return R;
     }
 
-    POLIcx operator - (POLIcx A, POLIcx B){
-        POLIcx R(A.getA()-B.getA(), A.getB()-B.getB(), A.getC()-B.getC(), 1);
+    complexPolinom operator - (complexPolinom A, complexPolinom B){
+        complexPolinom R(A.getA() - B.getA(), A.getB() - B.getB(), A.getC() - B.getC());
         if(A.getX()) R.setX(A.getX());
         else R.setX(B.getX());
         return R;
     }
 
-    POLIcx operator * (POLIcx A, POLIcx B){
-        POLIcx R(0,(A.getC()*B.getB())+(B.getC()*A.getB()), A.getC()*B.getC(),1);
+    complexPolinom operator * (complexPolinom A, complexPolinom B){
+        complexPolinom R(0, (A.getC() * B.getB()) + (B.getC() * A.getB()), A.getC() * B.getC());
         if (A.haveVar()) R.setX(A.getX());
         else R.setX(B.getX());
         return R;
     }
 
-    POLIcx operator / (POLIcx A, POLIcx B){
+    complexPolinom operator / (complexPolinom A, complexPolinom B){
         complex<double> zero=0;
         if (B.getC() == zero ) return A;
-        POLIcx R(0, A.getB() / B.getC(), A.getC() / B.getC(), 1);
+        complexPolinom R(0, A.getB() / B.getC(), A.getC() / B.getC());
         R.setX(A.getX());
         return R;
     }
 
-    POLIcx operator == (POLIcx A, POLIcx B){
-        POLIcx C = A - B;
+    complexPolinom operator == (complexPolinom A, complexPolinom B){
+        complexPolinom C = A - B;
         complex<double> zero=0, two = 2, four = 4;
         if (C.getA()==zero) C.getX()->img = -C.getC()/C.getB();
         else C.getX()->img = (-C.getB() + sqrt(pow(C.getB(), two) - (four*C.getA()*C.getC())))/(two*C.getA());
         return C;
     }
 
-    double solve(POLI r){return r.getX()->real;}
-    complex<double> solve(POLIcx x){return x.getX()->img;}
+    double solve(polinom r){return r.getX()->real;}
+    complex<double> solve(complexPolinom x){return x.getX()->img;}
 
 }
 
